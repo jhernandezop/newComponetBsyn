ef4b56ec495b6f4a13097abd508ee7263d466d9a
jhernandezop <52459238+jhernandezop@users.noreply.github.com>
2019-08-09 01:11:40 -0400

    timeline

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import ProcesoManual from './components/ProcesoManual';
 import Vertelefono from './components/VerTelefono';
 import Agenda from './components/Agenda';
 import Search from './components/Search';
-
+import Timelines from './components/Timeline';
+import moment from 'moment';
 
 //import Footer from './components/Footer';
 
@@ -27,6 +28,7 @@ class App extends Component {
        verTelefono: false,
        verAgenda:false,
        anexo:"",
+       tipoUsuario:"",
        ejecutivos:[],
        interfaz:"gestion",
        grupos:[],
@@ -36,6 +38,10 @@ class App extends Component {
                         {
                           opcion:"fab fa-trello", 
                           funcion:"gestion"
+                        },
+                        {
+                          opcion:"fas fa-chart-pie", 
+                          funcion:"reporte_superivisor"
                         },
                         {
                           opcion:"fas fa-chart-line", 
@@ -60,7 +66,7 @@ class App extends Component {
        edicion:[],
        procesomanual:[
                         {tag:"contacto", cantidad:0, ver: true, canales:[]},
-                        {tag:"cotizaciones", cantidad:0, ver: true, canales:[]},
+                        {tag:"nuevo", cantidad:0, ver: true, canales:[]},
                         {tag:"en gestion", cantidad:0, ver: true, canales:[]},
                         /*{
                           tag:"en gestion", 
@@ -105,6 +111,7 @@ class App extends Component {
     this.estadoAgenda=this.estadoAgenda.bind(this)
     this.estadoTelefono=this.estadoTelefono.bind(this)
     this.searchFiltro=this.searchFiltro.bind(this)
+    this.pedirFichas=this.pedirFichas.bind(this)
     
 
   }
@@ -116,8 +123,8 @@ class App extends Component {
     var url = 'https://bscore.openpartner.cl/gdm%22,%7B';
     var data = {
                   "tx"      : "getTs",
-                  "ts_o"    : "{{ ts_str }}",
-                  "tx_user"   : "3099",
+                  "ts_o"    : moment().format('YYYY-MM-DDTHH:mm:ss'),
+                  "tx_user"   : this.state.anexo,
                   "origen"    : "pruebas EFRAIN",
                   "columnas"  : ""
               };
@@ -143,9 +150,17 @@ class App extends Component {
 
                         //PARSEO DE DATOS 
                         if(element.estado=="nuevo"){
-                          element.estado="cotizaciones";
+                          element.estado="nuevo";
                         }else if(element.estado=="en_gestion"){
                           element.estado="en gestion";
+                        }else if(element.estado==""){
+                           element.estado="nuevo";
+                        }else if(element.estado=="seguimiento" || element.estado=="agendado propio" || element.estado=="sin respuesta"){
+                           element.estado="en gestion";
+                        }else if(element.canal=="web" && Number(element.nro_gestion)>=1){
+                           element.estado="en gestion";
+                        }else if(element.canal=="telefonia" && Number(element.nro_gestion)>=1){
+                           element.estado="en gestion";
                         }
                         
                         //element.tipo_caso="Seguimiento"
@@ -208,7 +223,20 @@ class App extends Component {
     .then(response => {
             
             if(response.estatus=="OK"){
+              const ejecutivos = [];
+              /*  response.data.forEach(function(element) {
+                  ejecutivos.push({
+                                    "label": element.ConsultorVentas,
+                                    "value": element.RUT
+                  })  
+
+                }); 
+
+            */
+
+
                this.setState({ejecutivos:response.data});
+
 
             }
             //console.log(this.state.ejecutivos)
@@ -270,6 +298,11 @@ class App extends Component {
     //this.setState({xmlOverlays: overlays});
     //ACTUALIZAR CANTIDADES DEL PROCESO
     const procesomanual=this.state.procesomanual
+    //SETEO PROCESO MANUAL A 0
+     procesomanual.forEach(function(element_a, index_a) {
+          element_a.cantidad=0;
+     })
+
 
     //RRECORRO LA FICHAS
     fichas.forEach(function(element_a, index_a) {
@@ -340,9 +373,12 @@ class App extends Component {
   }
 
 
-  desplegarEdicion(datosFormulario, caso_ES) {
+  desplegarEdicion(datosFormulario, ficha) {
     //console.log(datosFormulario)
-    this.setState({edicion:[{"caso_ES": caso_ES, "datosFormulario":datosFormulario}]});
+    
+     
+  
+    this.setState({edicion:[{"ficha": ficha, "datosFormulario":datosFormulario}]});
 
   }
 
@@ -442,7 +478,7 @@ class App extends Component {
 
 
     }else{
-      //this.setState({interfaz:opcion})
+      this.setState({interfaz:opcion})
 
     }
   }
@@ -508,7 +544,7 @@ class App extends Component {
                       </div>
                     </div>
                 </div>
-                <div className={this.state.expandida ? 'col-10' : 'col-12'}>
+                <div className={this.state.expandida ? 'col-8' : 'col-9'}>
                   <div className="row h-100">
                       <div id="indicadores" className="col-12 h-25">
                          <div className="row contenedor_indicadores">
@@ -570,14 +606,23 @@ class App extends Component {
                       </div>
                       <div className="col-12 h-75" >
                         <div className="row h-100">
-                          <div className="col-12 h-100">
-                                <AreaEdicion formulario={this.state.edicion} estadoAgenda={this.estadoAgenda}/> />
+                          <div   className="col-12 h-100">
+                                <AreaEdicion 
+                                  formulario={this.state.edicion} 
+                                  estadoAgenda={this.estadoAgenda}
+                                  ejecutivos={this.state.ejecutivos}
+                                  anexo={this.state.anexo}
+                                  pedirFichas={this.pedirFichas}
+                                /> />
 
                           </div>
                           
                         </div>
                       </div>
                     </div>
+                </div>
+                <div id="lineadetiempo" className={this.state.expandida ? 'col-2 h-100' : 'col-3 h-100'}>
+                  <Timelines  edicion={this.state.edicion}/>
                 </div>
             </div>
           </div>
@@ -596,13 +641,37 @@ class App extends Component {
             </div>
           </div>
           <div id="contenedor_app" className="row h-100 ">
-                <iframe src="https://ks2.openpartner.cl/s/gildemeister/app/kibana#/dashboard/c318d240-b4a8-11e9-8613-03e1f136a0bd?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3A%272019-07-01T00%3A00%3A00.000Z%27%2Cto%3A%272019-08-01T23%3A59%3A59.999Z%27))" height="100%" width="100%"></iframe>
+
+                <iframe src={"https://ks2.openpartner.cl/s/gildemeister/app/kibana#/dashboard/3d231760-ba0a-11e9-9b8e-b94dc0c5d9c8?embed=true&_g=(filters:!())&_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,field:doc_candidato_anexo,index:'1dc57010-b9f7-11e9-9b8e-b94dc0c5d9c8',key:doc_candidato_anexo,negate:!f,params:(query:'"+this.state.anexo+"'),type:phrase,value:'"+this.state.anexo+"'),script:(script:(lang:painless,params:(value:'"+this.state.anexo+"'),source:'boolean%20compare(Supplier%20s,%20def%20v)%20%7Breturn%20s.get()%20%3D%3D%20v;%7Dcompare(()%20-%3E%20%7B%20def%20nombre%3D%20doc%5B!'doc_co_lugaratencion.keyword!'%5D.value;%0D%0A%0D%0Aif%20(nombre%3D%3D%20%22AG20014%22)%20%7Breturn%20%223051%22%7D%0D%0Aif%20(nombre%3D%3D%20%22AG20015%22)%20%7Breturn%20%223003%22%7D%0D%0Aif%20(nombre%3D%3D%20%22AG20016%22)%20%7Breturn%20%223053%22%7D%0D%0Aif%20(nombre%3D%3D%20%22AG20023%22)%20%7Breturn%20%223001%22%7D%0D%0Aif%20(nombre%3D%3D%20%22AG20041%22)%20%7Breturn%20%223052%22%7D%20%0D%0Aif%20(nombre%3D%3D%20%22AG20057%22)%20%7Breturn%20%223006%22%7D%20%0D%0Aif%20(nombre%3D%3D%20%22AG20075%22)%20%7Breturn%20%223001%22%7D%0D%0Aif%20(nombre%3D%3D%20%22AG20076%22)%20%7Breturn%20%223006%22%7D%20%0D%0Aif%20(nombre%3D%3D%20%2225584175-0%22)%20%7Breturn%20%223099%22%7D%20%0D%0A%0D%0Aelse%20%7Breturn%20%223099%22%7D%20%0D%0A%0D%0A%20%7D,%20params.value);')))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:12,i:'1',w:7,x:0,y:0),id:'98c3edc0-ba09-11e9-9b8e-b94dc0c5d9c8',panelIndex:'1',title:'Casos%20del%20periodo',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:12,i:'2',w:19,x:7,y:0),id:'1a9a1000-ba13-11e9-9b8e-b94dc0c5d9c8',panelIndex:'2',title:'Estado%20de%20casos',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:6,i:'3',w:7,x:26,y:0),id:'889f5880-ba13-11e9-9b8e-b94dc0c5d9c8',panelIndex:'3',title:Contactabilidad,type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:6,i:'4',w:8,x:33,y:0),id:e443e2f0-ba13-11e9-9b8e-b94dc0c5d9c8,panelIndex:'4',title:Agendamiento,type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:6,i:'5',w:7,x:41,y:0),id:'57d4b1e0-ba14-11e9-9b8e-b94dc0c5d9c8',panelIndex:'5',title:'Casos%20Gestionados',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:6,i:'6',w:22,x:26,y:6),id:'877b38f0-ba15-11e9-9b8e-b94dc0c5d9c8',panelIndex:'6',title:'M%C3%A9tricas%20por%20sucursal',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:5,i:'7',w:7,x:0,y:12),id:'1e601150-ba16-11e9-9b8e-b94dc0c5d9c8',panelIndex:'7',title:'C.%20%20gestionados%20del%20periodo',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:5,i:'8',w:7,x:0,y:17),id:'9a969040-ba1c-11e9-9b8e-b94dc0c5d9c8',panelIndex:'8',title:'Total%20gestiones%20del%20periodo',type:visualization,version:'7.3.0'),(embeddableConfig:(vis:(legendOpen:!f)),gridData:(h:10,i:'9',w:19,x:7,y:12),id:'51305ce0-ba1e-11e9-9b8e-b94dc0c5d9c8',panelIndex:'9',title:'Resultado%20de%20gestion',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:10,i:'10',w:22,x:26,y:12),id:'51a9dd30-ba1f-11e9-9b8e-b94dc0c5d9c8',panelIndex:'10',title:'Gestiones%20por%20caso',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:9,i:'11',w:48,x:0,y:22),id:c09925a0-ba21-11e9-9b8e-b94dc0c5d9c8,panelIndex:'11',title:'Casos%20Ingresados%20Por%20D%C3%ADa%20y%20Gestionados',type:visualization,version:'7.3.0'),(embeddableConfig:(),gridData:(h:9,i:'12',w:48,x:0,y:31),id:'562e4c80-ba22-11e9-9b8e-b94dc0c5d9c8',panelIndex:'12',type:visualization,version:'7.3.0')),query:(language:kuery,query:''),timeRestore:!f,title:'FACE%200.3%20-%20EJECUTIVOS',viewMode:view)"} height="100%" width="100%"></iframe>
+              }
                
                 
           </div>
         </div>
       );
-    } 
+    }else if(this.state.estadoLogin==true &&  this.state.interfaz=="reporte_superivisor"){
+      return (
+        <div className="container-fluid h-100">
+          <div className="row">
+            <div className="col-12">
+                        <OpcioneDeNavegacion  
+                          opciones={this.state.opcionesOsuario} 
+                          interfazExpandida={this.interfazExpandida} 
+                          estado={this.state.expandida}
+                          navegarInterfaz={this.navegarInterfaz}
+                        />
+            </div>
+          </div>
+          <div id="contenedor_app" className="row h-100 ">
+
+                <iframe src={"https://ks2.openpartner.cl/s/gildemeister/app/kibana#/dashboard/82e55d50-ba3a-11e9-9b8e-b94dc0c5d9c8?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3A%272019-08-07T00%3A00%3A00.000Z%27%2Cto%3A%272019-08-09T23%3A59%3A59.999Z%27))"} height="100%" width="100%"></iframe>
+              }
+               
+                
+          </div>
+        </div>
+      );
+    }  
   }
 }
 
